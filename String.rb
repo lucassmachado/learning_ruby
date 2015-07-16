01fa534bf6c2fade49878e7157be228a8ca39c33
Lucas de Souza Machado <lucas.machado290@gmail.com>
2015-07-16 13:48:57 -0300

    getting started with ruby

---
+++ String.rb
@@ -0,0 +1,5 @@
+class String
+	def plural
+		"#{self}s"
+	end
+end
---
+++ loja_virtual/lib/livro.rb
@@ -0,0 +1,17 @@
+﻿# coding: utf-8
+class Livro
+	def initialize(autor, isbn = "1", numero_de_paginas, preco)
+		@autor = autor
+		@isbn = isbn
+		@numero_de_paginas = numero_de_paginas
+		@preco = preco
+	end
+
+	def preco
+		@preco
+	end
+
+	def to_s
+		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
+	end
+end
