99d6cde873c7253819fe3039ffa66b803e240c3e
Lucas de Souza Machado <lucas.machado290@gmail.com>
2015-07-17 15:17:50 -0300

    including biblioteca class and loja_virtual file to load the others classes

---
+++ loja_virtual/lib/biblioteca.rb
@@ -0,0 +1,14 @@
+class Biblioteca
+	def initialize
+		@livros = {}
+	end
+
+	def adiciona(livro)
+		@livros[livro.categoria] ||= []
+		@livros[livro.categoria] << livro
+	end
+
+	def livros
+		@livros.values.flatten
+	end
+end
--- loja_virtual/lib/livro.rb
+++ loja_virtual/lib/livro.rb
@@ -1,16 +1,18 @@
 ﻿# coding: utf-8
 class Livro
+	attr_accessor :valor
+	attr_reader :categoria
 
-	def initialize(autor, isbn = "1", numero_de_paginas, preco)
+	def initialize(autor, isbn = "1", numero_de_paginas, valor, categoria)
 		@autor = autor
 		@isbn = isbn
 		@numero_de_paginas = numero_de_paginas
-		@preco = preco
+		@valor = valor
+		@categoria = categoria
 	end
 
-	attr_accessor :preco
-
 	def to_s
-		"Autor: #{@autor}, Isbn: #{@isbn}, Páginas: #{@numero_de_paginas}"
+		"Autor: #{@autor}, Isbn: #{@isbn}, 
+			Páginas: #{@numero_de_paginas}, Categoria: #{@categoria}"
 	end
 end
